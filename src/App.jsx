ba1aa4292adb63f2ec807cff7535b11929ef61e8
dsefremov <dsefremof@gmail.com>
2024-03-17 20:02:54 +0300

    added Header and menu desktop & mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Buttons from "./components/Buttons";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Buttons className="mt-10" href="#login">
-          Something
-        </Buttons>
+        <Header />
       </div>
       <ButtonGradient />
     </>
